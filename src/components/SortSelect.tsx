1aafbbb14e197f275dca3cd14bfe585a26f8a77c
minjae0402 <kangjack0402@gmail.com>
2026-09-10 04:01:18 +0900

    Del : count

--- src/components/SortSelect.tsx
+++ src/components/SortSelect.tsx
@@ -18,8 +18,6 @@ export default function SortSelect({ sortOrder, onChange }: SortSelectProps) {
       >
         <option value="latest">최신순</option>
         <option value="oldest">오래된 순</option>
-        <option value="downloads_desc">다운로드 많은순</option>
-        <option value="downloads_asc">다운로드 적은순</option>
       </select>
     </div>
   );
